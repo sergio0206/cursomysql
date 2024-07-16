f26ba9bfa29ba1a7817b6838e5856f573c342a11
SERGIO ALEJANDRO DELLAN CEQUEA <sergio.7204@aluno.pr.senac.br>
2024-07-16 08:40:43 -0300

    Estrutura inicial das tabelas do banco de dados comercial

---
+++ bkp_tables_init.sql
@@ -0,0 +1,222 @@
+-- MySQL dump 10.13  Distrib 8.0.37, for Win64 (x86_64)
+--
+-- Host: localhost    Database: comercial
+-- ------------------------------------------------------
+-- Server version	8.0.37
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `comclien`
+--
+
+DROP TABLE IF EXISTS `comclien`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comclien` (
+  `n_numeclien` int NOT NULL AUTO_INCREMENT,
+  `c_codidlien` varchar(10) DEFAULT NULL,
+  `c_nomeclien` varchar(100) DEFAULT NULL,
+  `c_razaclien` varchar(100) DEFAULT NULL,
+  `d_dataclien` date DEFAULT NULL,
+  `c_cnpjclien` varchar(20) DEFAULT NULL,
+  `c_foneclien` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`n_numeclien`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comclien`
+--
+
+LOCK TABLES `comclien` WRITE;
+/*!40000 ALTER TABLE `comclien` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comclien` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comforne`
+--
+
+DROP TABLE IF EXISTS `comforne`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comforne` (
+  `n_numeforne` int NOT NULL AUTO_INCREMENT,
+  `c_codiforne` varchar(10) DEFAULT NULL,
+  `c_nomeforne` varchar(100) DEFAULT NULL,
+  `c_razaforne` varchar(100) DEFAULT NULL,
+  `c_foneforne` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`n_numeforne`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comforne`
+--
+
+LOCK TABLES `comforne` WRITE;
+/*!40000 ALTER TABLE `comforne` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comforne` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comivenda`
+--
+
+DROP TABLE IF EXISTS `comivenda`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comivenda` (
+  `n_numeivenda` int NOT NULL AUTO_INCREMENT,
+  `n_numevenda` int NOT NULL,
+  `n_numeprodu` int NOT NULL,
+  `n_valoivenda` float(10,2) DEFAULT NULL,
+  `n_qtdeivenda` int DEFAULT NULL,
+  `n_descivenda` float(10,2) DEFAULT NULL,
+  PRIMARY KEY (`n_numeivenda`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comivenda`
+--
+
+LOCK TABLES `comivenda` WRITE;
+/*!40000 ALTER TABLE `comivenda` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comivenda` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comprudo`
+--
+
+DROP TABLE IF EXISTS `comprudo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comprudo` (
+  `n_numeprudo` int NOT NULL AUTO_INCREMENT,
+  `c_codiprudo` varchar(20) DEFAULT NULL,
+  `c_descprudo` varchar(100) DEFAULT NULL,
+  `n_valoprodu` float(10,2) DEFAULT NULL,
+  `c_situprudo` varchar(1) DEFAULT NULL,
+  `n_numeforne` int DEFAULT NULL,
+  PRIMARY KEY (`n_numeprudo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comprudo`
+--
+
+LOCK TABLES `comprudo` WRITE;
+/*!40000 ALTER TABLE `comprudo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comprudo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comvenda`
+--
+
+DROP TABLE IF EXISTS `comvenda`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comvenda` (
+  `n_numevenda` int NOT NULL AUTO_INCREMENT,
+  `c_codivenda` varchar(10) DEFAULT NULL,
+  `n_numeclien` int NOT NULL,
+  `n_numeforne` int NOT NULL,
+  `n_numevende` int NOT NULL,
+  `n_valovenda` float(10,2) DEFAULT NULL,
+  `n_descvenda` float(10,2) DEFAULT NULL,
+  `n_totavenda` float(10,2) DEFAULT NULL,
+  `d_datavenda` date DEFAULT NULL,
+  PRIMARY KEY (`n_numevenda`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comvenda`
+--
+
+LOCK TABLES `comvenda` WRITE;
+/*!40000 ALTER TABLE `comvenda` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comvenda` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comvendas`
+--
+
+DROP TABLE IF EXISTS `comvendas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comvendas` (
+  `n_numevenda` int NOT NULL AUTO_INCREMENT,
+  `c_codivenda` varchar(10) DEFAULT NULL,
+  `n_numeclien` int NOT NULL,
+  `n_numeforne` int NOT NULL,
+  `n_numevende` int NOT NULL,
+  `n_valovenda` float(10,2) DEFAULT NULL,
+  `n_descvenda` float(10,2) DEFAULT NULL,
+  `n_totavenda` float(10,2) DEFAULT NULL,
+  `d_datavenda` date DEFAULT NULL,
+  PRIMARY KEY (`n_numevenda`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comvendas`
+--
+
+LOCK TABLES `comvendas` WRITE;
+/*!40000 ALTER TABLE `comvendas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comvendas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comvende`
+--
+
+DROP TABLE IF EXISTS `comvende`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comvende` (
+  `n_numevende` int NOT NULL AUTO_INCREMENT,
+  `c_codivende` varchar(10) DEFAULT NULL,
+  `c_nomevende` varchar(100) DEFAULT NULL,
+  `c_razavende` varchar(100) DEFAULT NULL,
+  `c_fonevende` varchar(20) DEFAULT NULL,
+  `n_porcvende` float(10,2) DEFAULT NULL,
+  PRIMARY KEY (`n_numevende`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comvende`
+--
+
+LOCK TABLES `comvende` WRITE;
+/*!40000 ALTER TABLE `comvende` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comvende` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-07-15 11:36:38
